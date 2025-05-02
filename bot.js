2dadfa729243e7d7647500da53e6ba06d38fe5b4
Mj65P <mojjtabajafari65@gmail.com>
2025-05-02 21:05:17 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // 🔹 **توکن جدید ربات خود را جایگذاری کن**
 const bot = new TelegramBot('7575583391:AAHqZ3bwxiKVPV2zCQouH8aZfy_b3VAdgP4', { polling: true });
-
 // 🔹 وقتی کاربر دستور /start را می‌فرستد، این پیام و دکمه نمایش داده می‌شود.
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "🎮 خوش آمدید به SkyBouncerBot! روی دکمه‌ی زیر کلیک کن تا بازی را شروع کنی:", {
